5c6632e1ba09a33d4096a62c095cc5664d7a3a3a
sinabin <sinabinvr94@gmail.com>
2023-10-02 21:53:05 +0900

    게임메카 batch 크롤러, daily 크롤러 별도 작성

--- Gamemeca_Batch.py
+++ Gamemeca_Batch.py
@@ -5,6 +5,7 @@ import hashlib
 from urllib.parse import urljoin
 from concurrent.futures import ThreadPoolExecutor
 import pymysql
+import utils
 
 def main():
     # 세션 생성 (TCP 연결 재사용)
@@ -24,7 +25,7 @@ def main():
         request_url=url+"?"+page
         print("현재 처리중인 page : ", page)
 
-        soup=parse_news_page(session,request_url)
+        soup=utils.parse_news_page(session,request_url)
         news_items=soup.find_all('li')
         news_list.extend(news_items) 
 
@@ -34,7 +35,7 @@ def main():
     conn, curs = None, None
 
     try:
-         conn, curs = connect_db()
+         conn, curs = utils.connect_db()
 
          with ThreadPoolExecutor(max_workers=10) as executor:
 
@@ -57,7 +58,7 @@ def main():
 
                      hashed_title=hashlib.sha256(title_text.encode()).hexdigest()
                   
-                     orign_news=parse_news_page(session,link_url)
+                     orign_news=utils.parse_news_page(session,link_url)
                    
                      article=str(orign_news.find('div',{'class':'article'}))
                    
@@ -67,7 +68,7 @@ def main():
                    
                      if img_element_in_atag: 
                         img_url=img_element_in_atag["src"]
-                        executor.submit(download_image,session,img_url,hashed_title)
+                        executor.submit(utils.download_image,session,img_url,hashed_title)
 
                         #DB Insert
                         sql = "insert into news_pages(news_id, news_title, news_desc,\
@@ -79,46 +80,7 @@ def main():
                         except Exception as e:
                             print(f"An error occurred while trying to insert data into the database: {str(e)}")
     finally:
-        close_db(conn)        
-
-def download_image(session,img_url ,hashed_title):
-   file_name=os.path.join("C:\\Users\\sinab\\IdeaProjects\\HappyGamse\\src\\main\\resources\\static\\imgs\\news",hashed_title+".jpg")
-   try:
-       response = session.get(img_url)
-       with open(file_name,"wb") as f: 
-           f.write(response.content) 
-   except requests.exceptions.RequestException as e:  
-       print(f"An error occurred while trying to get {img_url}: {str(e)}")
-   except Exception as e:
-       print(f"An error occurred while trying to write the file {file_name}: {str(e)}")
-
-
-def parse_news_page(session, url):
-    try:
-        response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred while trying to get {url}: {str(e)}")
-
-def connect_db():
-    conn = None
-    curs = None
-    try:
-        conn = pymysql.connect(host='127.0.0.1', user='root', password='lwss10233#', db='happyus', charset='utf8')
-        curs = conn.cursor(pymysql.cursors.DictCursor)
-    except Exception as e:
-        print(f"An error occurred while trying to connect to the database: {str(e)}")
-        
-    return conn, curs
-
-def close_db(conn):
-    if conn is not None:
-      try: 
-          conn.commit()
-          conn.close()
-      except Exception as e:
-          print(f"An error occurred while trying to close the database connection: {str(e)}")
+        utils.close_db(conn)        
 
 if __name__ == "__main__":
     main()
